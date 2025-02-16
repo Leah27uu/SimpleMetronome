943a4e9f39e43fafbdd830f50f144d6ae6ff2f5a
Leah <leah27u@gmail.com>
2025-02-16 16:45:57 +0100

    Edited folders and dlls to be in libs folder

--- SimpleMetronome/App.xaml.cs
+++ SimpleMetronome/App.xaml.cs
@@ -1,14 +1,32 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.IO;
+using System.Reflection;
+using System;
 
-namespace SimpleMetronome;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace SimpleMetronome
 {
-    
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += LoadFromLibsFolder;
+        }
+
+        private Assembly LoadFromLibsFolder(object sender, ResolveEventArgs args)
+        {
+            string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+            string libsFolder = Path.Combine(baseFolder, "libs");
+            string assemblyPath = Path.Combine(libsFolder, new AssemblyName(args.Name).Name + ".dll");
+
+            if (File.Exists(assemblyPath))
+            {
+                return Assembly.LoadFrom(assemblyPath);
+            }
+            return null;
+        }
+    }
 }
+
 
